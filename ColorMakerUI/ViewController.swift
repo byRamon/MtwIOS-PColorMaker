f0d985a1af4b67462430d447c0f24e9f50e7326b
ramon baeza yepez <By_ramon@outlook.com>
2020-01-25 13:44:35 -0600

    colores completados

--- ColorMakerUI/ViewController.swift
+++ ColorMakerUI/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var uiView: UIView!
+    var green = 0
+    var red = 0
+    var blue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeRed(_ sender: UISwitch) {
+        if sender.isOn{
+            self.red = 1
+        }
+        else{
+            self.red = 0
+        }
+        self.uiView.backgroundColor = UIColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue), alpha: 1)
+    }
+    @IBAction func changeGreen(_ sender: UISwitch) {
+        if sender.isOn{
+            green = 1
+        }
+        else{
+            green = 0
+        }
+        self.uiView.backgroundColor = UIColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue), alpha: 1)
+    }
+    @IBAction func changeBlue(_ sender: UISwitch) {
+        if sender.isOn{
+            blue = 1
+        }
+        else{
+            blue = 0
+        }
+        self.uiView.backgroundColor = UIColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue), alpha: 1)
+    }
 }
 
