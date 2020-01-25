2a1d1fe4258f5a28dc750dc26bbf811d999cb955
ramon baeza yepez <By_ramon@outlook.com>
2020-01-25 12:56:14 -0600

    Initial Commit

---
+++ ColorMakerUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMakerUI
+//
+//  Created by ByRamon on 1/25/20.
+//  Copyright © 2020 ByRamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
